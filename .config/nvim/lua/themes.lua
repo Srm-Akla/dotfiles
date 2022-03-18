3e29534db53e715b7090bedfa9d8c9f2ec57a025
Srm-Akla <sriramakella@protonmail.com>
2022-03-18 19:43:27 +0100

    merging lualine.lua and theme.lua in nvim config

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -15,27 +15,59 @@ require("packages")
 -- nightfox theme
 local nightfox = require('nightfox')
 
-nightfox.setup{
-  fox = "nightfox", -- Which fox style should be applied
-  transparent = true, -- Disable setting the background color
-  alt_nc = false, -- Non current window bg to alt color see `hl-NormalNC`
-  terminal_colors = true, -- Configure the colors used when opening :terminal
-  styles = {
-    comments = "italic", -- Style that is applied to comments: see `highlight-args` for options
-    functions = "italic,bold", -- Style that is applied to functions: see `highlight-args` for options
-    keywords = "italic,bold", -- Style that is applied to keywords: see `highlight-args` for options
-    strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
-    variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
-  },
-  inverse = {
-    match_paren = false, -- Enable/Disable inverse highlighting for match parens
-    visual = true, -- Enable/Disable inverse highlighting for visual selection
-    search = true, -- Enable/Disable inverse highlights for search highlights
-  },
-  colors = {}, -- Override default colors
-  hlgroups = {}, -- Override highlight groups
-}
+nightfox.setup({
+    options = {
+      -- fox = "duskfox", -- Which fox style should be applied
+      transparent = false, -- Disable setting the background color
+      alt_nc = false, -- Non current window bg to alt color see `hl-NormalNC`
+      terminal_colors = true, -- Configure the colors used when opening :terminal
+      styles = {
+        comments = "italic", -- Style that is applied to comments: see `highlight-args` for options
+        functions = "italic,bold", -- Style that is applied to functions: see `highlight-args` for options
+        keywords = "italic,bold", -- Style that is applied to keywords: see `highlight-args` for options
+        strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
+        variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
+      },
+      inverse = {
+        match_paren = false, -- Enable/Disable inverse highlighting for match parens
+        visual = true, -- Enable/Disable inverse highlighting for visual selection
+        search = true, -- Enable/Disable inverse highlights for search highlights
+      },
+      colors = {}, -- Override default colors
+      hlgroups = {}, -- Override highlight groups
+    }
+})
 
+-----------------------------------------------------------------------
+-- Lualine
+require('lualine').setup {
+	  options = {
+	    icons_enabled = false,
+	    -- theme = 'dayfox-nvim',
+	    component_separators = { left = '', right = ''},
+	    section_separators = { left = '', right = ''},
+	    disabled_filetypes = {},
+	    always_divide_middle = true,
+	  },
+	  sections = {
+	    lualine_a = {'mode'},
+	    lualine_b = {'branch'},
+	    lualine_c = {'filename'},
+	    lualine_x = {'encoding', 'fileformat', 'filetype'},
+	    lualine_y = {'progress'},
+	    lualine_z = {'location'}
+	  },
+	  inactive_sections = {
+	    lualine_a = {},
+	    lualine_b = {},
+	    lualine_c = {'filename'},
+	    lualine_x = {'location'},
+	    lualine_y = {},
+	    lualine_z = {}
+	  },
+	  tabline = {},
+	  extensions = {}
+}
 -----------------------------------------------------------------------
 -- Tokyo Night
 -- vim.g.tokyonight_style = "day"
@@ -51,5 +83,5 @@ vim.g.dracula_italic_comment = true
 
 -- nightfox.load()
 -- vim.o.background = 'light'
-vim.cmd[[colorscheme dayfox]]
+vim.cmd[[colorscheme duskfox]]
 
